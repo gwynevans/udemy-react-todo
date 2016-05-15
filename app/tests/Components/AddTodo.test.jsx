1b79d9d9b2c0d0a3330a0d831920627a30e31ade
gwyn <gwyn.evans@gmail.com>
2016-05-15 20:52:36 +0100

    Create AddTodo component

---
+++ app/tests/Components/AddTodo.test.jsx
@@ -0,0 +1,38 @@
+var React     = require('react');
+var ReactDOM  = require('react-dom');
+var expect    = require('expect');
+var $         = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+  it('should exist', () => {
+    expect(AddTodo).toExist();
+  });
+
+  it('should call onAddTodo if non-empty text', () => {
+    var todoText = 'Run unit tests';
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todoText.value = todoText;
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith(todoText);
+  });
+
+  it('should not call onAddTodo if empty text', () => {
+    var todoText = '';
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todoText.value = todoText;
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toNotHaveBeenCalled();
+  });
+
+});
