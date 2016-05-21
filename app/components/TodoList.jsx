3bbe3a5282c7bf9a63f73aba608ba38ca520300a
gwyn <gwyn.evans@gmail.com>
2016-05-21 16:58:05 +0100

    Style todo application

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -6,6 +6,11 @@ var TodoList = React.createClass({
     var {todos} = this.props;
 
     var renderTodos = () => {
+      if (todos.length === 0) {
+        return (
+          <p className="container__message">Nothing To Do - Awesome!</p>
+        )
+      }
       return todos.map((todo) => {
         return (
           <Todo onToggle={this.props.onToggle} key={todo.id} {...todo}/>
